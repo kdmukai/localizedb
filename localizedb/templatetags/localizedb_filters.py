1dc38819bfb75c848ab8765bfdf30fba5e01e344
Keith Mukai <keith.mukai@essaytagger.com>
2012-09-04 15:11:58 -0500

    Simplified the templatetag, added comments.

--- localizedb/templatetags/localizedb_filters.py
+++ localizedb/templatetags/localizedb_filters.py
@@ -1,12 +1,9 @@
 from django import template
 register = template.Library()
 
-
-
 @register.filter()
 def localize(value, arg):
     from localizedb.models import FieldGroup
-    from localizedb.models import TranslatedField
     field_group = value
     language_code = arg
     
@@ -14,15 +11,16 @@ def localize(value, arg):
         return ''
     
     #Get the TranslatedFields that go with this FieldGroup
-    translated_fields = TranslatedField.objects.filter(field_group=field_group)
+    translated_fields = field_group.translatedfield_set.filter(language_code=language_code)
     
-    if translated_fields and translated_fields.filter(language_code=language_code):
-        return translated_fields.filter(language_code=language_code)[0].translation
-    
-    elif translated_fields:
+    if translated_fields and translated_fields.count() > 0:
         return translated_fields[0].translation
     
+    # Fall back to the first TranslatedField entry if a translation for the requested
+    #    language_code is unavailable
+    elif field_group.translatedfield_set.all().count() > 0:
+        return field_group.translatedfield_set.all()[:1][0].translation
+    
+    # Fall back all the way to the FieldGroup's description in the Django admin
     else:
         return field_group.description
-
-
